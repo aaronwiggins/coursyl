0634f6680388f74c56ab6c4ace51879bb3ffe087
Aaron Wiggins <apw7177@gmail.com>
2015-06-16 23:13:18 -0400

    can delete rows, soft scroll to anchor points, fix multi submit bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,83 @@
 //= require jquery_ujs
 //= require d3
 //= require_tree .
+
+// stop buttom from submitting twice
+function disableButton() {
+  var button = document.getElementById("submit_link");
+  button.submit();
+  button.disabled = true;
+}
+
+var oneClick = disableButton;
+
+// // hide last row on page load
+window.onload = function() {
+  var section = document.getElementsByClassName("association container");
+  var last = section[section.length - 1];
+  last.style.display = "none";
+}
+
+// // show last row on add column button
+function showLastRow () {
+  var section = document.getElementsByClassName("association container");
+  var last = section[section.length - 1];
+  last.style.display = "block";
+  function disableButton() {
+    var button = document.getElementById("submit_link");
+    buttom.submit();
+    button.disabled = true;
+  };
+}
+
+// // delete row when delete button is pressed
+function hideRow (deleteButton) {
+  var deleteRow = deleteButton.parentElement.parentElement;
+  deleteRow.style.display = "none";
+  var elementForDelete = deleteButton.parentElement.lastElementChild;
+  checked = elementForDelete.checked = true;
+}
+
+// // stuff for soft scroll
+//
+// // current Y position
+// function currentYPosition() {
+//      if (self.pageYOffset) return self.pageYOffset;
+//      return 0;
+//  }
+//
+//  // element Y position
+// function elmYPosition(eID) {
+//     var elm = document.getElementById(eID);
+//     var y = elm.offsetTop;
+//     var node = elm;
+//     while (node.offsetParent && node.offsetParent != document.body) {
+//         node = node.offsetParent;
+//         y += node.offsetTop;
+//     } return y;
+// }
+//
+// // smooth scroll magic
+// function smoothScroll(eID) {
+//     var startY = currentYPosition();
+//     var stopY = elmYPosition(eID);
+//     var distance = stopY > startY ? stopY - startY : startY - stopY;
+//     if (distance < 100) {
+//         scrollTo(0, stopY); return;
+//     }
+//     var speed = Math.round(distance / 100);
+//     if (speed >= 20) speed = 20;
+//     var step = Math.round(distance / 25);
+//     var leapY = stopY > startY ? startY + step : startY - step;
+//     var timer = 0;
+//     if (stopY > startY) {
+//         for ( var i=startY; i<stopY; i+=step ) {
+//             setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+//             leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+//         } return;
+//     }
+//     for ( var i=startY; i>stopY; i-=step ) {
+//         setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+//         leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+//     }
+// }
